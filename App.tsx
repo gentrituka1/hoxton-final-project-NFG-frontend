bd4d21427edf72bdf9baeaf6146822d8ab671c73
Gentrit Uka <gentrit-uka@live.com>
2022-10-10 21:38:58 +0200

    updated

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.project}>This is my first Project!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  project: {
+    color: 'blue',
+    fontSize: 30,
+  }
 });
